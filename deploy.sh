7044e840231388e70bb5d9c30ee6bc28a856b472
Andy Aldridge <andy@grange85.co.uk>
2021-09-03 23:22:33 +0100

    fixed

--- deploy.sh
+++ deploy.sh
@@ -25,9 +25,14 @@ aws s3 sync --size-only --delete --exclude '.sass-cache' _deploy/ s3://www.grang
 # echo "update routing rules..."
 # aws s3api put-bucket-website --bucket www.fullofwishes.co.uk --website-configuration file://config/routing-rules.json
 
+# upload media
+echo "uploading media..."
+aws s3 sync --size-only --delete --exclude '.sass-cache' /home/andy/repos/grange85-media-cdn/ s3://cdn.grange85.co.uk
+
 # invalidate cloudfront
 echo "invalidate cloudfont distribution..."
-aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID --paths "/*"
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID_WWW --paths "/living-by-the-water/*"
+aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID_CDN --paths "/*"
 
 # ping feedburner
 curl --write-out 'pinged feedburner\n' --silent --output /dev/null "https://www.feedburner.com/fb/a/pingSubmit?bloglink=https%3A%2F%2Fwww.grange85.co.uk/"
