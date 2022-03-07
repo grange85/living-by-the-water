0c8313c227ed7543f2cc5d95cf7237dd46b64282
Andy Aldridge <andy@grange85.co.uk>
2022-03-07 22:50:15 +0000

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -27,12 +27,12 @@ aws s3 sync --size-only --delete --exclude '.sass-cache' _deploy/ s3://www.grang
 
 # upload media
 echo "uploading media..."
-aws s3 sync --size-only --delete --exclude '.sass-cache' /home/andy/repos/grange85-media-cdn/ s3://cdn.grange85.co.uk
+aws s3 sync --profile grange85 --size-only --delete --exclude '.sass-cache' /home/andy/repos/grange85-media-cdn/ s3://cdn.grange85.co.uk
 
 # invalidate cloudfront
 echo "invalidate cloudfont distribution..."
-aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID_WWW --paths "/living-by-the-water/*"
-aws cloudfront create-invalidation --distribution-id $CDN_DISTRIBUTION_ID_CDN --paths "/*"
+aws cloudfront create-invalidation --profile grange85 --distribution-id $CDN_DISTRIBUTION_ID_WWW --paths "/living-by-the-water/*"
+aws cloudfront create-invalidation --profile grange85 --distribution-id $CDN_DISTRIBUTION_ID_CDN --paths "/*"
 
 # ping feedburner
 curl --write-out 'pinged feedburner\n' --silent --output /dev/null "https://www.feedburner.com/fb/a/pingSubmit?bloglink=https%3A%2F%2Fwww.grange85.co.uk/"
